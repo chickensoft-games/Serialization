e68b7f6a22b4632e2571503feb1919d6c0638ee9
Joanna May <joannamadisonmay@gmail.com>
2024-08-23 19:08:04 -0500

    fix: support default values

---
+++ Chickensoft.Serialization.Tests/test/src/ModelWithEnumTest.cs
@@ -0,0 +1,76 @@
+namespace Chickensoft.Serialization.Tests;
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using Chickensoft.Serialization.Tests.Fixtures;
+using Shouldly;
+using Xunit;
+
+public class ModelWithEnumTest {
+  [Fact]
+  public void DeserializesAndRespectsDefaultValues() {
+    var value = /*lang=json,strict*/ """
+    {
+      "$type": "model_with_enum",
+      "$v": 1,
+      "c_type": "Complex"
+    }
+    """;
+
+    var options = new JsonSerializerOptions {
+      WriteIndented = true,
+      TypeInfoResolver = JsonTypeInfoResolver.Combine(
+        ModelWithEnumContext.Default,
+        new SerializableTypeResolver()
+      ),
+      Converters = {
+        new JsonStringEnumConverter<ModelWithEnum.ModelType>(),
+        new SerializableTypeConverter()
+      },
+    };
+
+    var result = JsonSerializer.Deserialize<ModelWithEnum>(value, options);
+
+    result.ShouldNotBeNull();
+
+    result.AType.ShouldBe(ModelWithEnum.ModelType.Basic); // default value
+    result.BType.ShouldBe(ModelWithEnum.ModelType.Advanced); // default value
+
+    result.CType.ShouldBe(ModelWithEnum.ModelType.Complex); // from JSON
+  }
+
+  [Fact]
+  public void DeserializesAndOverridesDefaultValues() {
+    var value = /*lang=json,strict*/ """
+    {
+      "$type": "model_with_enum",
+      "$v": 1,
+      "a_type": "Advanced",
+      "b_type": "Basic"
+    }
+    """;
+
+    var options = new JsonSerializerOptions {
+      WriteIndented = true,
+      TypeInfoResolver = JsonTypeInfoResolver.Combine(
+        ModelWithEnumContext.Default,
+        new SerializableTypeResolver()
+      ),
+      Converters = {
+        new JsonStringEnumConverter<ModelWithEnum.ModelType>(),
+        new SerializableTypeConverter()
+      },
+    };
+
+    var result = JsonSerializer.Deserialize<ModelWithEnum>(value, options);
+
+    result.ShouldNotBeNull();
+
+    result.AType.ShouldBe(ModelWithEnum.ModelType.Advanced); // default value
+    result.BType.ShouldBe(ModelWithEnum.ModelType.Basic); // default value
+
+    // missing json values should result in whatever is the 0 value of the enum
+    result.CType.ShouldBe(ModelWithEnum.ModelType.Basic);
+  }
+}
